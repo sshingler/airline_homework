f4ba9fcd0b58e1494207d086c95bc36b565e579a
SShingler <s.shingler@hotmail.co.uk>
2023-04-14 12:06:43 +0100

    Person hasName/getName

--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,20 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
+
+    Person person;
+
+    @Before
+    public void before() {
+        person = new Person ("Pete");
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Pete", person.getName());
+    }
+
 }
